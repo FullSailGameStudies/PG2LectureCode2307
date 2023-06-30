8ddab5c074dc88f9f4b79ddc207724109f7f7980
Garrett Girod <girodg@hotmail.com>
2023-06-30 09:35:10 -0400

    BatTheme

--- Day03/Day03/Program.cs
+++ Day03/Day03/Program.cs
@@ -77,35 +77,49 @@ namespace Day03
             */
             string file = "highScores";
             string postfile = PostFix(file); //if you don't pass a value, the default value will be used for the optional parameter
+            Console.WriteLine(postfile);
             postfile = PostFix(file, 5); //if a value is passed, it will be used for the optional parameter
-
+            Console.WriteLine(postfile);
 
 
             /*
                 CHALLENGE 1:
 
-                    Write a method called batTheme.
-                    Add an optional parameter to determine how many "na" are printed. The default value should be 13.
+                    Write a method called BatTheme.
+                    Add an optional parameter to determine how many "na" are printed. 
+                    The default value should be 13.
 
-                    If the calling code does not pass a value for the parameter, print "na na na na na na na na na na na na na Batman".
+                    If the calling code does not pass a value for the parameter, 
+                        print "na na na na na na na na na na na na na Batman".
                     If a value is passed, print the number of "na" equal to the value.
                     EX: if 6 is passed, print "na na na na na na Batman"
 
             */
-
-
+            BatTheme();
+            BatTheme(25);
 
 
             /*
                 CHALLENGE 2:
 
                     Write a ColorWriteLine method to print a message with a foreground color in the console.
-                    1) add a string message parameter AND an optional color parameter. Choose whatever default color you want.
+                    1) add a string message parameter AND an optional color parameter. 
+                        Choose whatever default color you want.
                     2) in the method, set the foreground color to the optional parameter
                     3) print the message
+                    4) reset the color
              
             */
 
+        }
+
+        static void BatTheme(int nas = 13)
+        {
+            for (int i = 0; i < nas; i++)
+            {
+                Console.Write("na ");
+            }
+            Console.WriteLine("Batman!");
         }
 
         static string PostFix(string fileName, int postFixNumber = 1) //postFixNumber is optional
