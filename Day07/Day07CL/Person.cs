05ab979f6a3ca5fbac95a32e0ca9b6685f376e05
Garrett Girod <girodg@hotmail.com>
2023-07-10 10:27:53 -0400

    challenge 3

--- Day07/Day07CL/Person.cs
+++ Day07/Day07CL/Person.cs
@@ -10,5 +10,17 @@ namespace Day07CL
     public class Person
     {
         private int _age;//declaring the variable
+
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                if (value >= 0 && value <= 120)
+                    _age = value;
+            }
+        }
+        public string Name { get; set; }
+
     }
 }
