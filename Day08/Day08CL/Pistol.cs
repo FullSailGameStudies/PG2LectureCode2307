fbec4732c2f10c8ea4b4cc840ac87ed13870c446
Garrett Girod <girodg@hotmail.com>
2023-07-12 09:57:10 -0400

    challenge 1

---
+++ Day08/Day08CL/Pistol.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day08CL
+{
+    public class Pistol : Weapon
+    {
+        public int Rounds { get; set; }
+        public int MagCapacity { get; set; }
+        public Pistol(int range, int damage, int magCapacity, int rounds) 
+            : base(range, damage)
+        {
+            MagCapacity = magCapacity;
+            Rounds = rounds;
+        }
+    }
+}
