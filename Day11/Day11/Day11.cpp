eb7a37e326c01eb42f16200fb4e09e8f57e0f013
Garrett Girod <girodg@hotmail.com>
2023-07-19 10:26:12 -0400

    intro to C++

---
+++ Day11/Day11/Day11.cpp
@@ -0,0 +1,83 @@
+// Day11.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;//is this ok?
+
+enum Powers
+{
+    Money = 10, 
+    Strength, 
+    Swimming
+};
+void PrintPower(Powers myPower);
+int main()
+{
+    Powers myPower = Money;
+    PrintPower(myPower);
+
+    //std -- standard namespace
+    //:: -- scope resolution operator
+    //<< -- insertion operator. inserting something to the output stream
+
+    //c++ way
+    cout << "Hello World!\n" << 5 << true << "\n";
+
+    //c way
+    printf("Hello %s %d\n", "Gotham", 5);
+
+    std::string best = "Batman";
+    char theBest[] = "The Bat";//adds a \0 (null terminator)
+    char meh[] = { 'A','q','u','a','m','a','n','\0'}; //does not add \0
+    cout << theBest << "\n" << meh << "\n";
+
+    for (int i = 0; i < sizeof(theBest)/sizeof(char); i++)
+    {
+        cout << theBest[i] << ".";
+    }
+    cout << "\n";
+
+    //auto == C# var
+    //range-based for loop
+    for (auto c : theBest)
+    {
+        cout << c << " ";
+    }
+    cout << "\n";
+
+    //seed the random generator
+    srand(time(NULL));
+
+    //get a random # 0-RAND_MAX (32767)
+    int rando = rand() % 101 ;//0-100 (inclusive)
+
+    //templates
+    vector<int> scores;//stack instance of the class
+    //vector<int>* highScore = new vector<int>();//heap instance
+    scores.push_back(5);//List.Add
+    scores.push_back(rand());
+    scores.push_back(rand());
+    scores.push_back(rand());
+    for (size_t i = 0; i < scores.size(); i++)
+    {
+        cout << scores[i] << "\n";
+    }
+    return 0;
+}
+void PrintPower(Powers myPower)
+{
+    switch (myPower)
+    {
+    case Money:
+        cout << "Money\n";
+        break;
+    case Strength:
+        break;
+    case Swimming:
+        break;
+    default:
+        break;
+    }
+}
--- Day11CSharp/Day11CSharp/Program.cs
+++ Day11CSharp/Day11CSharp/Program.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿//using System;
 
 namespace Day11CSharp
 {
     internal class Program
     {
+        enum Powers
+        {
+            Money = 10,
+            Strength,
+            Swimming
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Console.Write("Hello World!\n");
+            Powers myPower = Powers.Money;
+            switch (myPower)
+            {
+                case Powers.Money:
+                    break;
+                case Powers.Strength:
+                    break;
+                case Powers.Swimming:
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
